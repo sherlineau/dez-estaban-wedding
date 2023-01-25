b11a8120ef04b5c5801290c782d97a9633580c82
Sherline Au <au.sherline@gmail.com>
2023-01-24 18:38:01 -0600

    changed rsvp button and font size

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,10 +4,10 @@ import img from '../../assets/images/IMG_9089.jpg'
 
 const Home = () => {
   return (
-    <div className='container-center'>
+    <div className='container-center home'>
       <img src={img} alt="dez and estaban horse-back riding"/>
-      <div>
-        <a href='https://docs.google.com/forms/d/e/1FAIpQLSfyWGwLNA-AK4-wQ-EMb6KGQpeCKMNmxqSPYn-0vybsQhqjSw/viewform?usp=sf_link' className='rsvp' target="_blank" rel="noreferrer">RSVP</a>
+      <div className='rsvp'>
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLSfyWGwLNA-AK4-wQ-EMb6KGQpeCKMNmxqSPYn-0vybsQhqjSw/viewform?usp=sf_link' className='rsvp-btn' target="_blank" rel="noreferrer">RSVP</a>
         <p>May 13, 2023 @ 12:30PM</p>
       </div>
     </div>
