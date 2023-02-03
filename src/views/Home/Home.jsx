b124e54ac2736c30ec7da355ad3ef38aca3ba61b
Sherline Au <au.sherline@gmail.com>
2023-02-03 17:08:20 -0600

    feat: added heading to home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,6 +5,7 @@ import img from '../../assets/images/IMG_9089.jpg'
 const Home = () => {
   return (
     <div className='container-center home'>
+      <h1>#unfinished</h1>
       <img src={img} alt="dez and estaban horse-back riding"/>
       <div className='rsvp'>
         <a href='https://docs.google.com/forms/d/e/1FAIpQLSfyWGwLNA-AK4-wQ-EMb6KGQpeCKMNmxqSPYn-0vybsQhqjSw/viewform?usp=sf_link' className='rsvp-btn' target="_blank" rel="noreferrer">RSVP</a>
