63ed87bd3db2921077b73070f25e4bd562c3e17e
Sherline Au <au.sherline@gmail.com>
2023-02-05 17:33:10 -0600

    fix: typos

--- src/views/Memento/Memento.jsx
+++ src/views/Memento/Memento.jsx
@@ -7,7 +7,7 @@ const Memento = () => {
     <div className="container-center">
       <div className="memento">
         <div className="content">
-          <p>Dear friends and family, as we look back on our special day, we would love to relive the joy and laughter through your lens. Please share any candid photos and videos you captured at our wedding by clicking the button below.  Your memories are a treasuer to us and we are forever grateful for them.  Thank you for being a part of our journey. ❤️ </p>
+          <p>Dear friends and family, as we look back on our special day, we would love to relive the joy and laughter through your lens. Please share any candid photos and videos you captured at our wedding by clicking the image below.  Your memories are a treasure to us and we are forever grateful for them.  Thank you for being a part of our journey. ❤️ </p>
         </div>
         <a
           href=" https://albums.memento.com/unfinished"
