bf14448b2747bb5655f61db1dccd579e8a7f245e
Sherline Au <au.sherline@gmail.com>
2023-02-05 16:02:01 -0600

    fix: typo

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <img src={img} alt="dez and estaban horse-back riding"/>
       <div className='rsvp'>
         <a href='https://docs.google.com/forms/d/e/1FAIpQLSfyWGwLNA-AK4-wQ-EMb6KGQpeCKMNmxqSPYn-0vybsQhqjSw/viewform?usp=sf_link' className='rsvp-btn' target="_blank" rel="noreferrer">RSVP</a>
-        <p>May 13, 2023 @ 12:30PM</p>
+        <p>May 13, 2023 @ 1:30PM</p>
       </div>
     </div>
   )
