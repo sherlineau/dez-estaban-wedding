f4db429cbba75027d605fa88b99546b2bbedb60f
Sherline Au <au.sherline@gmail.com>
2023-02-13 17:05:28 -0600

    remove button after rsvp date has passed

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
       <h1>#unfinished</h1>
       <img src={img} alt="dez and estaban horse-back riding" />
       <div className="rsvp">
-        <button className={today > rsvp ? 'rsvp-btn greyed':'rsvp-btn'}>
           {today > rsvp ? (
-            <p className="closed">RSVP by April 13</p>
+            ""
           ) : (
+            <button className="rsvp-btn">
             <a
               href="https://docs.google.com/forms/d/e/1FAIpQLSfyWGwLNA-AK4-wQ-EMb6KGQpeCKMNmxqSPYn-0vybsQhqjSw/viewform?usp=sf_link"
               target="_blank"
@@ -21,8 +21,8 @@ const Home = () => {
             >
               RSVP by April 13
             </a>
+            </button>
           )}
-        </button>
         <p>May 13, 2023 @ 1:00PM</p>
       </div>
     </div>
